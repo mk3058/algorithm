b79c7f84b6993b46cffa097904e852d77e39e96d
mk3058 <kmk3058@icloud.com>
2022-04-09 14:17:11 +0000

    22.04.09

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,32 +1,16 @@
-//1259
 package project;
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+    public void solution(){
 
-        while(true){
-            String input = scanner.nextLine();
-            if(input.equals("0")) break;
-            if(check_num(input)==0) System.out.println("yes");
-            else System.out.println("no");
-        }
-
-        scanner.close();
     }
 
-    static int check_num(String num){
-        int i=0;
-        int length = num.length();
-
-        for(i=0;i<length/2;i++){
-            if(num.charAt(i)!=num.charAt(length-1-i)){
-                return 1;
-            }
-        }
-
-        return 0;
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Main main = new Main();
+        
+        scanner.close();
     }
 }
