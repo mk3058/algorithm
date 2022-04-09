108a232c2ab7c387d86ef4ba65f898b627ec8ee8
mk3058 <kmk3058@icloud.com>
2022-04-09 05:32:33 +0000

    test commit

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,13 +1,32 @@
+//1259
 package project;
 
-public class Main {
+import java.util.Scanner;
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Hello goorm!");
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("0")) break;
+            if(check_num(input)==0) System.out.println("yes");
+            else System.out.println("no");
+        }
+
+        scanner.close();
     }
 
+    static int check_num(String num){
+        int i=0;
+        int length = num.length();
+
+        for(i=0;i<length/2;i++){
+            if(num.charAt(i)!=num.charAt(length-1-i)){
+                return 1;
+            }
+        }
+
+        return 0;
+    }
 }
