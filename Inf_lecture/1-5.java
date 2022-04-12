b3e2a2899f76cb2e17b60d3788865c3e8dc0194e
mk3058 <kmk3058@icloud.com>
2022-04-12 14:15:41 +0000

    22.04.12

--- Inf_lecture/1-5.java
+++ Inf_lecture/1-5.java
@@ -3,6 +3,44 @@ package project;
 import java.util.Scanner;
 
 public class Main {
+    public String solution(String input){
+        String ans="";
+        int lt=0;
+        int rt=input.length()-1;
+        char[] temp = input.toCharArray();
+
+        while(rt>lt){
+            char tmp;
+
+            if(!Character.isAlphabetic(temp[lt]))
+            lt++;
+            else if(!Character.isAlphabetic(temp[rt]))
+            rt--;
+            else{
+                tmp=temp[lt];
+                temp[lt]=temp[rt];
+                temp[rt]=tmp;
+                lt++;
+                rt--;
+            }
+        }
+        ans=String.valueOf(temp);
+        return (ans);
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Main main = new Main();
+
+        System.out.printf("%s", main.solution(scanner.nextLine()));
+        scanner.close();
+    }
+}
+
+/* package project;
+
+import java.util.Scanner;
+
+public class Main {
     public void solution(String str){
         String ans = "";
         char[] alp = new char[100];
@@ -37,4 +75,4 @@ public class Main {
         main.solution(scanner.next());
         scanner.close();
     }
-}
+} */
